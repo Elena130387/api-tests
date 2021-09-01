c34fd1946239288e770c61f8afe21c13f7fac3ac
SergeyArtiushin <Sergey.Artiushin@syncretis.com>
2021-09-01 16:10:23 +0700

    add create folder Reports

--- helper/exportToJsonFile.ts
+++ helper/exportToJsonFile.ts
@@ -1,5 +1,10 @@
 export const toJsonFile = (jsonData: any, name: string = "") => {
   const fs = require("fs");
   let data = JSON.stringify(jsonData);
-  fs.writeFileSync(`reports/performanceReport${name}.json`, data);
+  if (fs.existsSync(`reports`)) {
+    fs.writeFileSync(`reports/performanceReport${name}.json`, data);
+  } else {
+    fs.mkdirSync("reports");
+    fs.writeFileSync(`reports/performanceReport${name}.json`, data);
+  }
 };
