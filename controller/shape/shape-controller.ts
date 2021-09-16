fe4e6c572f7af88e68f7ffec40eac556d491e348
SergeyArtiushin <Sergey.Artiushin@syncretis.com>
2021-09-16 17:53:27 +0700

    added deleted spec

--- controller/shape/shape-controller.ts
+++ controller/shape/shape-controller.ts
@@ -1,46 +1,73 @@
-
-import {getShapesCountQuery} from "../../helper/urls";
-import {callRestApi, getRestBody} from "../api.controller";
-import {simpleShapeObject} from "../../requests/shape-resource/createNewShape";
+import { getShapesCountQuery } from "../../helper/urls";
+import { callRestApi, getRestBody } from "../api.controller";
+import { simpleShapeObject } from "../../requests/shape-resource/createNewShape";
 import { renameShape } from "../../requests/shape-resource/renameShape";
 
-const {MAIN_URL, DEMO_URL} = process.env
-const CALCULATION = `/calculation-management/shapes`
-export const CALCULATION_URL = process.env.TEST_ENV ? `${DEMO_URL}${CALCULATION}` : `${MAIN_URL}${CALCULATION}`
+const { MAIN_URL, DEMO_URL } = process.env;
+const CALCULATION = `/calculation-management/shapes`;
+export const CALCULATION_URL = process.env.TEST_ENV
+  ? `${DEMO_URL}${CALCULATION}`
+  : `${MAIN_URL}${CALCULATION}`;
 
+export const getShapeById = (shapeId: number) =>
+  callRestApi(`${CALCULATION_URL}/${shapeId}`, getRestBody("GET", null));
+export const getShapeByCount = (count: number) =>
+  callRestApi(
+    CALCULATION_URL,
+    getRestBody("GET", null, getShapesCountQuery(count))
+  );
+export const deleteShapeById = (shapeId: number) =>
+  callRestApi(`${CALCULATION_URL}/${shapeId}`, getRestBody("DELETE", null));
+export const createShape = (
+  name: string,
+  forceProcessing = false,
+  testDataGenerating = true
+) =>
+  callRestApi(
+    CALCULATION_URL,
+    getRestBody(
+      "POST",
+      simpleShapeObject(name, forceProcessing, testDataGenerating)
+    )
+  );
+export const renameShapeById = (shapeId: number, name: string) =>
+  callRestApi(
+    `${CALCULATION_URL}/${shapeId}`,
+    getRestBody("PUT", renameShape(name))
+  );
 
-export const getShapeById = (shapeId: number) => callRestApi(`${CALCULATION_URL}/${shapeId}`, getRestBody('GET', null))
-export const getShapeByCount = (count:number) => callRestApi(CALCULATION_URL, getRestBody('GET', null, getShapesCountQuery(count)))
-export const deleteShapeById = (shapeId: number) =>  callRestApi(`${CALCULATION_URL}/${shapeId}`, getRestBody('DELETE', null))
-export const createShape = (name: string, forceProcessing = false, testDataGenerating = true) =>  callRestApi(CALCULATION_URL, getRestBody('POST', simpleShapeObject(name, forceProcessing, testDataGenerating)))
-export const renameShapeById = (shapeId: number, name: string) =>  callRestApi(`${CALCULATION_URL}/${shapeId}`, getRestBody('PUT', renameShape(name)))
+export async function waitWhenAllProcessDone(shapeId: number) {
+  await new Promise((r) => setTimeout(r, 1000));
 
-export async function waitWhenAllProcessDone (shapeId: number) {
-    await new Promise((r) => setTimeout(r, 1000));
+  const response = await getShapeById(shapeId);
+  const { total, completed } = response.progress;
 
-    const response = await getShapeById(shapeId)
-    const {total, completed} = response.progress
+  if (total > 0 && total === completed) {
+    expect(total).toEqual(completed);
 
-    if(total > 0 && total === completed) {
-        expect(total).toEqual(completed)
-
-        return
-    }
-    await waitWhenAllProcessDone(shapeId)
+    return;
+  }
+  await waitWhenAllProcessDone(shapeId);
 }
 
-export async function waitWhenProcessStopped (shapeId: number) {
-    const STATUS = 'stopped'
+export async function waitWhenProcessStopped(shapeId: number) {
+  const STATUS = "stopped";
 
-    await new Promise((r) => setTimeout(r, 1000));
+  await new Promise((r) => setTimeout(r, 1000));
 
-    const response = await getShapeById(shapeId)
-    const {status, polygons} = response
+  const response = await getShapeById(shapeId);
+  const { status, polygons } = response;
 
-    if(status === STATUS && polygons[0].status === STATUS) {
-       ;[status, polygons[0].status].forEach(el => expect(el).toEqual(STATUS))
+  if (status === STATUS && polygons[0].status === STATUS) {
+    [status, polygons[0].status].forEach((el) => expect(el).toEqual(STATUS));
 
-        return
-    }
-    await waitWhenProcessStopped(shapeId)
+    return;
+  }
+  await waitWhenProcessStopped(shapeId);
 }
+const getAllShapesByName = async (count: number, name: string) =>
+  (await getShapeByCount(count)).filter((el: any) => el.name.includes(name));
+export const getAllidFromSortListShapesByName = async (
+  count: number,
+  name: string
+) => (await getAllShapesByName(count, name)).map((el: any) => el.id);
