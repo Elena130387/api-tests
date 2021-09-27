24c9dd14ddaeb99bd35c8f784b034041a460fe08
SergeyArtiushin <Sergey.Artiushin@syncretis.com>
2021-09-27 15:49:43 +0700

    remade regression test> comparison summary now, not graphQL

--- tests/regressionTest/chechWithReference.spec.ts
+++ tests/regressionTest/chechWithReference.spec.ts
@@ -9,37 +9,36 @@ import { referensShape } from "../../requests/shape-resource/createNewShape";
 import { getListWithErorrsValue } from "../../helper/jsonProcessing";
 import { toFile, toFileReferenceData } from "../../helper/exportFile";
 import { reportHTML } from "../../helper/createReports/checkRegefenceDataReportHTML";
+import {
+  getIdsExecutions,
+  getSummaryDataAboutJobs,
+} from "../../controller/job-execution/job-executions-controller";
 
 describe("comparison of reference data", function () {
   const percentError = 5;
-
   Object.keys(referenceData).forEach((el: any) => {
-    it.concurrent(
-      `test ${el}`,
-      async function () {
-        const NAME = `${
-          el[0].toUpperCase() + el.slice(1)
-        }. Comparison of with reference data: ${DATE}`;
-        const { id } = await createShape(NAME, true, false, referensShape[el]);
-        await waitWhenShapeStatusEqual(id);
-        const response = await getShape(id);
-        const { summary } = response.data.shape;
-        // @ts-ignore
-        const objReferenceData = referenceData[el].data.shape.summary;
-        toFile(
-          reportHTML(objReferenceData, response, percentError),
-          "htmlResponse",
-          "html"
-        );
-        toFileReferenceData(
-          `export const ${el}: any =${JSON.stringify(response)}`,
-          el
-        );
-        expect(
-          getListWithErorrsValue(objReferenceData, summary, percentError).length
-        ).toEqual(0);
-      },
-      1000000
-    );
+    it(`test ${el}`, async function () {
+      const NAME = `${
+        el[0].toUpperCase() + el.slice(1)
+      }. Comparison of with reference data: ${DATE}`;
+      const { id } = await createShape(NAME, false, true, referensShape[el]);
+      await waitWhenShapeStatusEqual(id);
+      const idExecutions = await getIdsExecutions(id);
+      const response = await getSummaryDataAboutJobs(idExecutions[0]);
+      // @ts-ignore
+      const objReferenceData = referenceData[el];
+      toFile(
+        reportHTML(objReferenceData, response, percentError, id, NAME),
+        "htmlResponse",
+        "html"
+      );
+      toFileReferenceData(
+        `export const ${el}: any =${JSON.stringify(response)}`,
+        el
+      );
+      expect(
+        getListWithErorrsValue(objReferenceData, response, percentError).length
+      ).toEqual(0);
+    }, 1000000);
   });
 });
