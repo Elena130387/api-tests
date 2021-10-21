bbaf04c7dc28c29d47ca2d2d19cb9b487e1f75df
Sergey Artiushin <Sergey.Artiushin@syncretis.com>
2021-10-21 03:29:39 +0000

    Merged PR 2138: add new time for progress shape

--- tests/shape-resource/progressShape.spec.ts
+++ tests/shape-resource/progressShape.spec.ts
@@ -16,5 +16,5 @@ describe("wait when progress shape will done", function () {
 
   it("successfully completed progress", async function () {
     await waitWhenAllProcessDone(shapeId);
-  }, 50000);
+  }, 100000);
 });
