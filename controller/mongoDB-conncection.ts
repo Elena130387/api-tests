17b3e3c20f7d92455ee5e41d2fd149a7996e5516
SergeyArtiushin <Sergey.Artiushin@syncretis.com>
2021-12-24 19:44:00 +0700

    update image_service test

--- controller/mongoDB-conncection.ts
+++ controller/mongoDB-conncection.ts
@@ -3,25 +3,15 @@ export const urlMongoDB =
   "mongodb://exro:exro-dev@51.124.136.209:27017/?authSource=exro-imageservice-db&readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false";
 const client = new MongoClient(urlMongoDB);
 const dbName = "exro-imageservice-db";
+const db = client.db(dbName);
+const collection = db.collection("satellite-images");
 
-export const connectMongoDB = async () => {
-  await client.connect();
-};
+export const connectMongoDB = async () => await client.connect();
 
-export const disconnectMongoDB = async () => {
-  await client.close();
-};
+export const disconnectMongoDB = async () => await client.close();
 
-export const findByKeyInMongoDB = async (key: string) => {
-  const db = client.db(dbName);
-  const collection = db.collection("satellite-images");
-  const elements = await collection.find({ key: key }).toArray();
+export const findByKeyInMongoDB = async (key: string) =>
+  await collection.find({ key: key }).toArray();
 
-  return elements;
-};
-
-export const removeByKeyInMongoDB = async (key: string) => {
-  const db = client.db(dbName);
-  const collection = db.collection("satellite-images");
+export const removeByKeyInMongoDB = async (key: string) =>
   await collection.deleteMany({ key: key });
-};
