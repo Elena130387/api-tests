62d72fcfabd1b6df8cf596e413e4aa931e97dbb4
Sergey Artiushin <Sergey.Artiushin@syncretis.com>
2021-12-24 12:40:32 +0000

    Merged PR 2468: update image_service test

--- tests/api_tests/imageService/getTileByImageServise.spec.ts
+++ tests/api_tests/imageService/getTileByImageServise.spec.ts
@@ -1,12 +1,10 @@
 import { getImageTile } from "../../../controller/imageService/image-service-controller";
 import {
+  connectMongoDB,
+  disconnectMongoDB,
   findByKeyInMongoDB,
   removeByKeyInMongoDB,
-  urlMongoDB,
 } from "../../../controller/mongoDB-conncection";
-
-const { MongoClient } = require("mongodb");
-const client = new MongoClient(urlMongoDB);
 
 describe("check serviceImage", function () {
   const keyForSearching = "38.38_-110.4_38.39_-110.41_16";
@@ -17,8 +15,14 @@ describe("check serviceImage", function () {
     lon_se: -110.4,
   };
   const mapProviders = ["bing", "google"];
+  beforeAll(async function () {
+    await connectMongoDB();
+  });
+  afterAll(async function () {
+    await disconnectMongoDB();
+  });
   afterEach(async function () {
-    await removeByKeyInMongoDB(keyForSearching).finally(() => client.close());
+    await removeByKeyInMongoDB(keyForSearching);
   });
 
   it.each(mapProviders)(
@@ -26,7 +30,6 @@ describe("check serviceImage", function () {
     async function (provider) {
       await getImageTile(coord, provider);
 
-      //
       const response = await findByKeyInMongoDB(keyForSearching);
 
       expect(response.length).toEqual(1);
